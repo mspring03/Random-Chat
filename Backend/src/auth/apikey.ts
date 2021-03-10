de8d29b401bd434a8861cd68abb6415ca3c79ede
mspring03 <mspringmspring@naver.com>
2021-03-10 23:37:50 +0900

    [CREATE] </auth/apikey.ts> inspection apikey

---
+++ Backend/src/auth/apikey.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { ForbiddenError } from '../core/apiError';
+import { PublicRequest } from 'app-request';
+import asyncHandler from '../middleware/asyncHandler';
+import { apiKey } from '../config';
+import Logger from '../core/Logger';
+import validator, { ValidationSource } from '../middleware/validator';
+import schema from '../schema/auth';
+
+const router = express.Router();
+
+export default router.use(
+  validator(schema.apiKey, ValidationSource.HEADER),
+  asyncHandler(async (req: PublicRequest, res, next) => {
+    req.apiKey = req.headers['x-api-key'].toString();
+
+    Logger.info(req.apiKey);    
+
+    if (req.apiKey !== apiKey) throw new ForbiddenError();
+
+    return next();
+  }),
+);
