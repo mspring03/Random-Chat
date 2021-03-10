9283bbe022cc63d77560b193e69a4afff91937a7
mspring03 <mspringmspring@naver.com>
2021-03-10 23:45:14 +0900

    [CREATE] <routes/index.ts>

---
+++ Backend/src/routes/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import apikey from '../auth/apikey';
+
+const router = express.Router();
+
+// router.use('/', apikey);
+
+router.use('/test', );
+
+export default router;
