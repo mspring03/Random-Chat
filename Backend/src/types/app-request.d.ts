743f7fb6d980e3a59b804050d452b90b844c4dd9
mspring03 <mspringmspring@naver.com>
2021-03-10 23:44:43 +0900

    [CREATE] </types/app-requeset.d.ts>

---
+++ Backend/src/types/app-request.d.ts
@@ -0,0 +1,21 @@
+import { types } from '@hapi/joi';
+import { StdioPipe } from 'child_process';
+import { Request } from 'express';
+import { Application } from 'express';
+
+declare interface PublicRequest extends Request {
+  apiKey: string;
+}
+
+declare interface RoleRequest extends PublicRequest {
+  currentRoleCode: string;
+}
+
+declare interface ProtectedRequest extends RoleRequest {
+  accessToken: string;
+}
+
+declare interface Tokens {
+  accessToken: string;
+  refreshToken: string;
+}
