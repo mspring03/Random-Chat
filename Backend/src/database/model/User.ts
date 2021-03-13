c7efba2ee27be28e9003be39511a91ea9eaf65c7
mspring03 <mspringmspring@naver.com>
2021-03-14 04:29:22 +0900

    [MODIFY] </model/User.ts>

--- Backend/src/database/model/User.ts
+++ Backend/src/database/model/User.ts
@@ -6,47 +6,15 @@ const User = new mongoose.Schema({
     id: { type: String, required: true },
     password: { type: String, required: true },
     nickname: { type: String, required: true, unique: true },
-    description: { type: String},
+    description: { type: String },
     tag: { type: String },
     connection: { type: Boolean },
-    Last_access_time: { type: Date },
+    guest: { type: Boolean },
+    socket: { type: String },
+    Last_access_time: { type: String },
     created_at: { type: Date, default: Date.now }
 }, {
     versionKey: false 
 });
 
 export default mongoose.model("user", User);
-// import {
-//     Entity,
-//     PrimaryGeneratedColumn,
-//     Column,
-//     CreateDateColumn,
-//   } from 'typeorm';
-  
-//   @Entity()
-//   export class User {
-//     @PrimaryGeneratedColumn()
-//     uuid: number;
-
-//     @Column()
-//     id: string;
-
-//     @Column()
-//     nickname: string;
-
-//     @Column()
-//     description: string;
-
-//     @Column()
-//     tag: string;
-
-//     @Column()
-//     connection: boolean;
-
-//     @Column()
-//     Last_access_time: string;
-  
-//     @CreateDateColumn({ type: 'timestamp' })
-//     createdAt: string;
-// }
-  
