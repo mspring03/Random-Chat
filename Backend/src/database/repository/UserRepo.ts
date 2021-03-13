805e9388f0ec983e95bd9d145f9e0b11c578cc02
mspring03 <mspringmspring@naver.com>
2021-03-14 04:29:50 +0900

    [ADD] </repository/UserRepo.ts> user repository

---
+++ Backend/src/database/repository/UserRepo.ts
@@ -0,0 +1,49 @@
+import User from '../model/User';
+
+class UserRepository {
+    public static async findById(id: string): Promise<Object> {
+        return await User.findOne()
+            .where('id').equals(id)
+    }
+
+    public static async findByNickname(nickname: string): Promise<Object> {
+        return await User.findOne()
+            .where('nickname').equals(nickname)
+    }
+
+    public static async createUser(
+        id: string,
+        password: string,
+        nickname: string,
+        description: string,
+        tag: string,
+    ) {
+        User.create({
+            id: id,
+            password: password,
+            nickname: nickname,
+            description: description,
+            tag: tag,
+            connection: false,
+            guest: false,
+            Last_access_time: Date.now().toString(),
+        })
+    }
+
+    public static async createguestUser(
+        id: string,
+        password: string,
+        nickname: string,
+    ) {
+        User.create({
+            id: id,
+            password: password,
+            nickname: nickname,
+            connection: false,
+            guest: true,
+            Last_access_time: Date.now().toString(),
+        })
+    }
+}
+
+export default UserRepository;
