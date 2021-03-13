4cfad54f03b9dd9e89e151e964c77490e466b444
mspring03 <mspringmspring@naver.com>
2021-03-14 04:33:34 +0900

    [CREATE] </schema/user.ts>

---
+++ Backend/src/schema/user.ts
@@ -0,0 +1,24 @@
+import Joi from '@hapi/joi';
+import { JoiAuthBearer } from '../middleware/validator';
+
+export default {
+  userCredential: Joi.object().keys({
+    id: Joi.string().required().min(6),
+    password: Joi.string().required().min(6),
+  }),
+  guest: Joi.object().keys({
+    nickname: Joi.string().required().min(2)
+  }),
+  auth: Joi.object()
+  .keys({
+    authorization: JoiAuthBearer().required(),
+  })
+  .unknown(true),
+  signup: Joi.object().keys({
+      id: Joi.string().required().min(4),
+      password: Joi.string().required().min(6),
+      nickname: Joi.string().required().min(2),
+      description: Joi.string(),
+      tag: Joi.string(),
+  })
+}
