0ff8f0cdb852a8d4e7ccf3d54f1cbcaf1ce9d9d2
mspring03 <mspringmspring@naver.com>
2021-03-14 04:31:10 +0900

    [ADD] <routers/index.ts> adding auth-related routers

--- Backend/src/routes/index.ts
+++ Backend/src/routes/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
 import apikey from '../auth/apikey';
+import test from './user/test';
+import signup from './user/signup'
+import signin from './user/signin'
+import guestLogin from './user/guestLogin'
 
 const router = express.Router();
 
 // router.use('/', apikey);
 
-router.use('/test', );
+router.use('/test', test);
+router.use('/signup', signup);
+router.use('/signin', signin);
+router.use('/guest', guestLogin)
 
 export default router;
