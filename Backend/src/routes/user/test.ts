f236d24fa208f462797d2eedbd7777a12a2ed0f5
mspring03 <mspringmspring@naver.com>
2021-03-14 04:33:13 +0900

    test

--- Backend/src/routes/user/test.ts
+++ Backend/src/routes/user/test.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import validator from '../../middleware/validator';
 import { SuccessResponse } from '../../core/apiResponse';
 import asyncHandler from '../../middleware/asyncHandler';
-import { RoleRequest } from 'app-request';
 import crypto from 'crypto';
 import _ from 'lodash';
 import { environment } from '../../config';
@@ -11,11 +10,25 @@ import {
   BadRequestError,
   AuthFailureError,
 } from '../../core/apiError';
+import User from '../../database/model/User';
+import ChatRoom from '../../database/model/ChatRoom'
+import UserRepository from '../../database/repository/UserRepo';
+import ChatRoomRepository from '../../socket/socketFunction/ChatRoomRepo'
 
 const router = express.Router();
 
 router.post('/', 
-    asyncHandler(async () => {
+  asyncHandler(async (req, res) => {
+    console.log(await ChatRoom.find());
 
-    })
+    // const user = await ChatRoomRepository.userMatching('game')
+    // console.log(user);
+    
+    console.log(await ChatRoom.find());
+    
+
+    res.status(200).json({ message: "success" })
+  })
 );
+
+export default router
