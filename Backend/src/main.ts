9f932c4a4509d72c562d7161df79f2506939959a
mspring03 <mspringmspring@naver.com>
2021-03-14 04:30:17 +0900

    [MODIFY] </src/main.ts>

--- Backend/src/main.ts
+++ Backend/src/main.ts
@@ -1,11 +1,18 @@
 import App from './loaders'
+import { port } from './config'
+import Logger from './core/Logger'
 
 const app = new App
 
-async function main():Promise<void>{
-    await app.run()
-    app.initMiddlewares()
-    app.initRoutes()
-    await app.initDb()
-}
-main();
+app.listen(port, ()=> {
+    Logger.info(`Listening on port ${port}`)
+})
+
+// async function main():Promise<void>{
+//     await app.run();
+//     app.initMiddlewares();
+//     app.initRoutes();
+//     app.errorHandler();
+//     await app.initDb();
+// }
+// main();
