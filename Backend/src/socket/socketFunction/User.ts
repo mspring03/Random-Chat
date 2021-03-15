57c36df26fa8df81a958e1198d02c35e2679808f
mspring03 <mspringmspring@naver.com>
2021-03-15 16:18:54 +0900

    [MODIFY] </socketFunction/User.ts>

--- Backend/src/socket/socketFunction/User.ts
+++ Backend/src/socket/socketFunction/User.ts
@@ -9,6 +9,18 @@ class UserRepository {
         return User.findOne()
             .where('socket').equals(socket);
     }
+
+    public static async joinRoom(socket, roomName) {
+        User.update({ socket: socket }, { $set: { accessRoom: roomName }})
+    }
+
+    public static async outRoom(roomName) {
+        User.update({ accessRoom: roomName }, { $set: { accessRoom: "" }})
+    }
+
+    public static async disconnect(socket) {
+        User.findOneAndUpdate({ socket: socket }, { connection: false, socket: "", accessRoom: "" }, { new: false })
+    }
 }
 
 export default UserRepository;
