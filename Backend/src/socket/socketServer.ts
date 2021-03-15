ac6df0421af3885e5a376c9f277ed0ef5e9372b0
mspring03 <mspringmspring@naver.com>
2021-03-15 16:19:10 +0900

    [MODIFY] </socket/socketServer.ts>

--- Backend/src/socket/socketServer.ts
+++ Backend/src/socket/socketServer.ts
@@ -1,5 +1,6 @@
 import UserRepository from './socketFunction/User'
 import ChatRoomRepository from './socketFunction/ChatRoomRepo';
+import ChatLogRepository from './socketFunction/ChatLogRepo';
 import { Server as IO } from 'socket.io';
 
 export default (io: IO, socket) => {
@@ -26,11 +27,45 @@ export default (io: IO, socket) => {
         const matchedUser = await ChatRoomRepository.userMatching(socket, tag);
         const myInfo = await UserRepository.FindBySocketID(socket);
         const userInfo = await UserRepository.FindBySocketID(matchedUser['socket']);
-        const roomName = `${myInfo['nickname']}&${userInfo['nickname']}`
+        
+        const chatLog = await ChatLogRepository.findChatLog(myInfo['nickname'], userInfo['nickname']); 
+        let roomName = chatLog['roomName'];
+
+        if (!chatLog) {
+            roomName = `${myInfo['nickname']}&${userInfo['nickname']}`;
+            ChatLogRepository.createChatRoom(roomName)
+        }
 
         matchedUser['socket'].join(roomName);
         myInfo['socket'].join(roomName);
 
+        UserRepository.joinRoom(socket, roomName);
+        UserRepository.joinRoom(matchedUser['socket'], roomName)
+
         io.sockets.to(roomName).emit('randomUserFindingCompete', roomName)
+    })
+
+    socket.on('message', async (result) => {
+        io.sockets.to(result['roomName']).emit('message', { data: result['data'], nickname: result['nickname'] });
+        ChatLogRepository.saveChatingLog(result['roomName'], result['data'], result['nickname'])
+    })
+
+    socket.on('roomClosing', async (roomName) => {
+        io.sockets.to(roomName).emit("chatEnd");
+    });
+
+    socket.on('roomClear', async (roomName) => {
+        UserRepository.outRoom(roomName);
+    })
+
+    socket.on('disconnect', async () => {
+        const User = UserRepository.disconnect(socket);
+        if(User['roomName']) io.sockets.to(User['roomName']).emit("chatEnd");
+
+        const ChatRoom = await ChatRoomRepository.goOutRoom(socket);
+        if(ChatRoom) {
+            ChatRoomRepository.ChangedAllRoomPeople(io);
+            ChatRoomRepository.ChangedRoomPeople(io, ChatRoom['tag']);
+        }
     })
 }
