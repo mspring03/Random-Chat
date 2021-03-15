aaa04782896a63832e834255bd71e3b2b35109ee
mspring03 <mspringmspring@naver.com>
2021-03-15 16:17:53 +0900

    [MODIFY] test

--- Backend/src/routes/user/test.ts
+++ Backend/src/routes/user/test.ts
@@ -14,17 +14,38 @@ import User from '../../database/model/User';
 import ChatRoom from '../../database/model/ChatRoom'
 import UserRepository from '../../database/repository/UserRepo';
 import ChatRoomRepository from '../../socket/socketFunction/ChatRoomRepo'
+import ChatLog from '../../database/model/ChatLog'
+import { info } from 'winston';
 
 const router = express.Router();
 
 router.post('/', 
   asyncHandler(async (req, res) => {
-    console.log(await ChatRoom.find());
+    console.log(await ChatLog.find());
 
-    // const user = await ChatRoomRepository.userMatching('game')
+  //   UserSchema.methods.addFriend = function (info) {
+  //     this.friendList.push({friendId: info.id, name: info.name});
+  //     return this.save();
+  // };
+    // const user = await ChatLog.create({
+    //   roomName: "망나뇽&기모찌맨"
+    // })
     // console.log(user);
+    // await ChatLog.update({roomName: "망나뇽&기모찌맨"}, {$push: {chating: {message: "무야호", time: Date()}}});
+
+    console.log(await ChatLog.findOne().and([
+      { roomName: { $regex: '.*' + '망나뇽' + '.*' } }, 
+      { roomName: { $regex: '.*' + '기모찌맨' + '.*' } }
+    ]));
+
+
+
+    //   { roomName: { $regex: '.*' + '망나뇽' + '.*' } }, 
+    //   // { roomName: { $regex: '.*' + '기모찌맨' + '.*' } }
+    // ));
     
-    console.log(await ChatRoom.find());
+
+    // console.log(await ChatLog.findOne({ roomName: '망나뇽&기모찌맨' }));
     
 
     res.status(200).json({ message: "success" })
