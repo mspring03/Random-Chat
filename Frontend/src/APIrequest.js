c45bab6393303d3b2bce28870fcf4ef257e3ecb9
mspring03 <mspringmspring@naver.com>
2021-03-20 23:36:17 +0900

    [CREATE] </src/APIrequest.js>

---
+++ Frontend/src/APIrequest.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+const BASE_URL = 'http://localhost';
+
+export const requestApi = async (url, body, header, method) => {
+	try {
+		const res = await axios({
+			url: BASE_URL + url,
+			data: body,
+			headers: header,
+			method,
+		});
+		return res;
+	} catch (err) {
+		if (err.response) {
+			throw err.response.status;
+		}
+		alert('네트워크 연결을 확인해 주세요');
+		throw null;
+	}
+};
+
+
