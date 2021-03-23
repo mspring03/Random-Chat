c28ceff994ad021880a9242b28932589798ecbea
mspring03 <mspringmspring@naver.com>
2021-03-23 12:00:11 +0900

    [CREATE] </GuestLoginContainer/GuestLoginContainer.js>

---
+++ Frontend/src/containers/GuestLoginContainer/GuestLoginContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import GuestLogin from '../../component/User/GuestLogin/GuestLogin';
+
+const GuestLoginContainer = () => {
+    return <GuestLogin />
+}
+
+export default GuestLoginContainer;
