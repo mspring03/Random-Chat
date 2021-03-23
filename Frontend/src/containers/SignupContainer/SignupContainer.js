09162a74d0a28e1af41b557a6472a5797c6b74f0
mspring03 <mspringmspring@naver.com>
2021-03-23 12:00:36 +0900

    [CREATE] </SignupContainer/SignupContainer.js>

---
+++ Frontend/src/containers/SignupContainer/SignupContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import Signup from '../../component/User/Signup/Signup';
+
+const SignupContainer = () => {
+    return <Signup />
+}
+
+export default SignupContainer;
