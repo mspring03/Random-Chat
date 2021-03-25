4a03cfe7b0cb92f3416cee94791f4efed4e49a34
mspring03 <mspringmspring@naver.com>
2021-03-25 23:21:01 +0900

    [MODIFY] <socket/index.ts>

--- Backend/src/socket/index.ts
+++ Backend/src/socket/index.ts
@@ -1,16 +1,16 @@
-import socketioJwt from 'socketio-jwt';
 import { Server as IO } from 'socket.io';
-import { tokenInfo } from '../config'
 import socketServer from './socketServer';
 
 export default (io: IO) => {
-    io.sockets
-        .on('connection', socketioJwt.authorize({
-            secret: tokenInfo.key,
-            timeout: 15000 
-        }))
-        .on('authenticated', (socket) => {
-            socketServer(io, socket);
-            console.log(`hello! ${socket.decoded_token.name}`);
-        });
+    io.on('connection', (socket) => {
+        console.log(socket.id);
+    console.log('-----------------');
+            
+        
+        socket.on('data', () => {
+            console.log(socket.id);
+            
+        })
+        socketServer(io, socket);
+    });
 } 
