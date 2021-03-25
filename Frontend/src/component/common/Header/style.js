b8bbe607d5ac1e3c4329f5a0c90374a6ab026aa4
mspring03 <mspringmspring@naver.com>
2021-03-25 23:25:22 +0900

    [CREATE] </Header/style.js>

---
+++ Frontend/src/component/common/Header/style.js
@@ -0,0 +1,14 @@
+import styled, {css} from 'styled-components';
+
+export const Container = styled.div`
+    height:100vh;
+    display:flex;
+    width: 60%;
+    height:12.5%;
+`
+
+export const Formbody = styled.div`
+    height: 100%;
+    width: 100%;
+    margin-right: 15%;
+`;
