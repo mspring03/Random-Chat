b7c5546aa70a1c606b0161a5b4231fd2a93701ca
mspring03 <mspringmspring@naver.com>
2021-03-25 23:25:44 +0900

    [CREATE] </Room/style.js>

---
+++ Frontend/src/component/common/Room/style.js
@@ -0,0 +1,23 @@
+import styled, {css} from 'styled-components';
+
+export const FormWrap = styled.ul`
+    padding: 0px;
+    margin: 13px 0px 13px 0px;
+    overflow-y: scroll;
+    height: 100%;
+`;
+
+export const Formbutton = styled.div`
+    display: flex;
+    margin-left: 10%;
+    margin-bottom: 13px;
+    height: 70px;
+    background-color: #EBEBEB;
+    border-radius:15px;
+    align-items: center;
+    justify-content:center;
+`;
+
+export const Formimage = styled.img`
+    height: 40px
+`;  
