094b55895396bac9676d1466275ec7f1ffc1bad2
mspring03 <mspringmspring@naver.com>
2021-03-25 23:23:56 +0900

    [MODIFY] </src/App.js>

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -2,7 +2,8 @@ import React from 'react';
 import {Route, BrowserRouter} from 'react-router-dom';
 import Login from './containers/LoginContainer/LoginContainer';
 import Signup from './containers/SignupContainer/SignupContainer';
-import guestLogin from './containers/GuestLoginContainer/GuestLoginContainer'
+import GuestLogin from './containers/GuestLoginContainer/GuestLoginContainer'
+import Main from './containers/MainContainer/MainContainer'
 import GlobalStyle from './GlobalStyle';
 
 const App = () => {
@@ -10,8 +11,9 @@ const App = () => {
         <BrowserRouter>
         <GlobalStyle />
             <Route path='/login' component={Login} />
-            <Route path='/guestLogin' component={guestLogin} />
+            <Route path='/guestLogin' component={GuestLogin} />
             <Route path='/signup' component={Signup} />
+            <Route path='/main' component={Main} />
         </BrowserRouter>
     );
 
