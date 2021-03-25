b1b5fd9ce6f90d09142faa18886824144829c266
mspring03 <mspringmspring@naver.com>
2021-03-25 23:27:55 +0900

    [MODIFY] </MainContainer/Maincontainer.js>

--- Frontend/src/containers/MainContainer/MainContainer.js
+++ Frontend/src/containers/MainContainer/MainContainer.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-// impor
+import FriendList from '../../component/Friend/FriendList/FriendList';
+import RoomList from '../../component/RoomList/RoomList';
+import Header from '../../component/common/Header/Header';
 
 const MainContainer = () => {
-    return 
+    return (
+        <React.Fragment>
+            <FriendList />
+            <Header />
+            <RoomList />
+        </React.Fragment>
+    )
 }
 
 export default MainContainer;
