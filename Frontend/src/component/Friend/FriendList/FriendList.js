68d56a25393d98bbb06d94aadabad0bc492c7570
mspring03 <mspringmspring@naver.com>
2021-03-25 23:26:05 +0900

    [CREATE] </FriendList/FriendList.js>

---
+++ Frontend/src/component/Friend/FriendList/FriendList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import * as S from "./style";
+
+
+const FriendList = () => {
+    return (
+        <S.Container>
+            반갑고
+        </S.Container>
+    )
+}
+
+export default FriendList;
