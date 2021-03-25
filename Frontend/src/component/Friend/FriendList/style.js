a0a699f0a81f0539f04042a5d47f1b852a86fadf
mspring03 <mspringmspring@naver.com>
2021-03-25 23:26:18 +0900

    [CREATE] </FriendList/style.js>

---
+++ Frontend/src/component/Friend/FriendList/style.js
@@ -0,0 +1,9 @@
+import styled, {css} from 'styled-components';
+
+export const Container = styled.div`
+    height:100%;
+    display:flex;
+    background-color:#EBEBEB;
+    float: left;
+    width: 40%;
+`
