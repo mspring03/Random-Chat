620fc99b45a0351864ab4fce6c76bcfa5ec9d115
mspring03 <mspringmspring@naver.com>
2021-03-25 23:25:08 +0900

    [CREATE] </Header/Header.js>

---
+++ Frontend/src/component/common/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as S from './style'
+
+const Header = () => {
+    return (
+        <S.Container>
+            dfadf
+        </S.Container>
+    )
+}
+
+export default Header
