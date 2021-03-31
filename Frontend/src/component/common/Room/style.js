e887c6e0abdd48ce6a6757286dbe7821be7e44c4
mspring03 <mspringmspring@naver.com>
2021-03-31 23:55:20 +0900

    .

--- Frontend/src/component/common/Room/style.js
+++ Frontend/src/component/common/Room/style.js
@@ -18,6 +18,40 @@ export const Formbutton = styled.div`
     justify-content:center;
 `;
 
+export const FormInput = styled.input`
+    border-radius: 5px;
+    width: 290px;
+    padding: 1px 5px 0px 5px;
+`;
+
 export const Formimage = styled.img`
-    height: 40px
+    height: 40px;
 `;  
+
+export const FormBoxFrame = styled.li`
+    display: flex;
+    justify-content: space-between;
+    margin-left: 10%;
+    margin-bottom: 13px;
+    height: 70px;
+    background-color: #EBEBEB;
+    border-radius:15px;
+    align-items: center;
+`;
+
+export const Formname = styled.div`
+    padding-top: 3px;
+    margin-left: 23px;
+    font-weight: 600;
+    font-size: 20px;
+    font-family:Noto Sans;
+    float: left;
+`;
+
+export const Formpeople = styled.div`
+    /* padding-top: 3px; */
+    margin-right: 23px;
+    font-size: 17px;
+    color: #868686;
+    font-family:Noto Sans;
+`;
