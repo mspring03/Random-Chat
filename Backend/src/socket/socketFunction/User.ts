8a443f0f0c38de91215d70ce6ce4117ab7c8d8a3
mspring03 <mspringmspring@naver.com>
2021-03-31 23:55:15 +0900

    .

--- Backend/src/socket/socketFunction/User.ts
+++ Backend/src/socket/socketFunction/User.ts
@@ -2,7 +2,7 @@ import User from '../../database/model/User';
 
 class UserRepository {
     public static async UserStatesUpdate(socket, id: string) {
-        User.update({ id: id }, { $set: { connection: true, socket: socket }})
+        return await User.findOneAndUpdate({ id: id }, { $set: { connection: true, socket: socket }}, { new: true });
     }
 
     public static async FindBySocketID(socket) {
@@ -11,15 +11,19 @@ class UserRepository {
     }
 
     public static async joinRoom(socket, roomName) {
-        User.update({ socket: socket }, { $set: { accessRoom: roomName }})
+        await User.findOneAndUpdate({ socket: socket }, { $set: { accessRoom: roomName }})
     }
 
     public static async outRoom(roomName) {
-        User.update({ accessRoom: roomName }, { $set: { accessRoom: "" }})
+        await User.findOneAndUpdate({ accessRoom: roomName }, { $set: { accessRoom: "" }})
+    }
+
+    public static async deleteUser(id) {
+        await User.findOneAndRemove({ id: id });
     }
 
     public static async disconnect(socket) {
-        User.findOneAndUpdate({ socket: socket }, { connection: false, socket: "", accessRoom: "" }, { new: false })
+        return await User.findOneAndUpdate({ socket: socket }, { $set: { connection: false, socket: "", accessRoom: "" }}, { new: false })
     }
 }
 
