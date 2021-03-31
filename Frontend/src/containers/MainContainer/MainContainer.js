0bc6911b1e601a28f27f7dc6918d391f00c5c061
mspring03 <mspringmspring@naver.com>
2021-03-31 23:55:48 +0900

    .

--- Frontend/src/containers/MainContainer/MainContainer.js
+++ Frontend/src/containers/MainContainer/MainContainer.js
@@ -3,8 +3,15 @@ import React from 'react';
 import FriendList from '../../component/Friend/FriendList/FriendList';
 import RoomList from '../../component/RoomList/RoomList';
 import Header from '../../component/common/Header/Header';
+import { useHistory } from 'react-router-dom';
 
 const MainContainer = () => {
+    const history = useHistory();
+    
+    window.onpopstate = function() {
+        history.go(0)
+    }
+
     return (
         <React.Fragment>
             <FriendList />
