664fc1ff9c11ba472bca706e1ea93e298e2f807e
mspring03 <mspringmspring@naver.com>
2021-04-02 10:51:25 +0900

    ⭐️

--- Frontend/src/component/common/Header/style.js
+++ Frontend/src/component/common/Header/style.js
@@ -8,7 +8,27 @@ export const Container = styled.div`
 `
 
 export const Formbody = styled.div`
+    display: flex;
+    box-sizing: border-box;
+    justify-content: flex-end;
     height: 100%;
     width: 100%;
     margin-right: 15%;
+    padding: 5% 0px;
+`;
+
+export const FormBox = styled.div`
+    margin-left: 4%;
+    margin-right: 4%;
+    font-size: 22px;
+    margin-top: 5px;
+    font-weight: 500;
+    &:hover {
+        cursor: pointer;
+    }
+
+    & + & + &{
+        color: #4843C4;
+        margin-right: 3%;
+    }
 `;
