2fd7aefd39317aa719327025786421ba03c94c55
mspring03 <mspringmspring@naver.com>
2021-04-07 10:33:18 +0900

    ⭐️

--- Frontend/src/component/Friend/FriendList/FriendList.js
+++ Frontend/src/component/Friend/FriendList/FriendList.js
@@ -5,7 +5,47 @@ import * as S from "./style";
 const FriendList = () => {
     return (
         <S.Container>
-            반갑고
+            <S.Formbody>
+                <S.FormTop>
+                    <S.FormName>
+                        랜덤챗
+                    </S.FormName>
+                </S.FormTop>
+                <S.FormMainBox>
+                    <S.FormList>
+                        <S.FormBoxFrame>
+                            <S.FormBox>
+                                <S.profileImg>
+
+                                </S.profileImg>
+                                <S.FormUser>
+                                    <S.FormUser>
+                                        <S.UserNickName>
+                                            망나나망   
+                                        </S.UserNickName>
+                                        <S.chating>
+                                            동해물과 백두산이 마르고 닳도록
+                                        </S.chating>
+                                    </S.FormUser>
+                                    <S.UserState>
+                                        
+                                    </S.UserState>
+                                </S.FormUser>
+                            </S.FormBox>
+                        </S.FormBoxFrame>
+                        <S.FormBoxFrame>
+                            <S.FormBox>
+
+                            </S.FormBox>
+                        </S.FormBoxFrame>
+                        <S.FormBoxFrame>
+                            <S.FormBox>
+
+                            </S.FormBox>
+                        </S.FormBoxFrame>
+                    </S.FormList>
+                </S.FormMainBox>
+            </S.Formbody>
         </S.Container>
     )
 }
