2b5cd3aa5b931393cd24f8e6b6e432e13191e750
mspring03 <mspringmspring@naver.com>
2021-05-03 22:51:43 +0900

    .

--- Backend/src/socket/index.ts
+++ Backend/src/socket/index.ts
@@ -3,6 +3,11 @@ import socketServer from './socketServer';
 
 export default (io: IO) => {
     io.on('connection', (socket) => {
+        socket.on("test", () => {
+            console.log(123);
+            
+        })
         socketServer(io, socket); 
+        
     });
 } 
